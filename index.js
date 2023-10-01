c681038fbaf3171d8e69b60bab566a502ff4b603
henry9405 <144359211+henry9405@users.noreply.github.com>
2023-10-01 20:16:13 +0300

    fetch test

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  // Make a fetch request to the Game of Thrones API
+  const apiUrl = 'https://anapioficeandfire.com/api/books';
+
+  // Return the fetch promise so that it can be accessed by tests
+  return fetch(apiUrl)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then((data) => {
+      // Call the renderBooks function with the JSON data
+      renderBooks(data);
+    })
+    .catch((error) => {
+      console.error('Error fetching books:', error);
+    });
 }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
-  books.forEach(book => {
+  books.forEach((book) => {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   fetchBooks();
 });
+
